58c6a5cca649eb1cd4e78005ed136f8acf815e08
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-06-25 21:14:55 +0200

    create tests in class BlogManagerTest and repair method AddLikeToPost

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogManager.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogManager.java
@@ -43,6 +43,9 @@ public class BlogManager extends DomainService implements BlogService {
         if (post.getUser().getId().equals(userId)) {
             throw new DomainError("cannot like own post");
         }
+        if(user.getAccountStatus() != AccountStatus.CONFIRMED){
+            throw new DomainError("Only the user with the status of a CONFIRMED can like the post");
+        }
         Optional<LikePost> existingLikeForPost = likePostRepository.findByUserAndPost(user, post);
         if (existingLikeForPost.isPresent()) {
             return false;
--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,5 +1,9 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,6 +22,10 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -25,19 +33,55 @@ public class BlogManagerTest {
     @MockBean
     UserRepository userRepository;
 
+    @MockBean
+    LikePostRepository likePostRepository;
+
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
     @Autowired
     DataMapper dataMapper;
 
     @Autowired
     BlogService blogService;
 
+    private void createAll (AccountStatus accountStatus){
+        User firstUser = new User();
+        firstUser.setId(1L);
+        User userConfirmed = new User();
+        userConfirmed.setAccountStatus(accountStatus);
+        userConfirmed.setId(2L);
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(firstUser);
+        when(userRepository.findOne(2L)).thenReturn(userConfirmed);
+        when(blogPostRepository.findOne(1l)).thenReturn(blogPost);
+        when(likePostRepository.findByUserAndPost(userConfirmed, blogPost)).thenReturn(Optional.empty());
+    }
+    
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
-        blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
+        blogService.createUser(new UserRequest("Jan", "Kowalski", "jankkowalski@domain.com"));
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test
+    public void confirmedUserCanLikePost() throws Exception {
+        createAll(AccountStatus.CONFIRMED);
+        Assert.assertThat(blogService.addLikeToPost(2L, 1L), Matchers.is(true));
+    }
+
+    @Test(expected = DomainError.class)
+    public void NewUserCannotLikePost() throws Exception {
+        createAll(AccountStatus.NEW);
+        blogService.addLikeToPost(2L, 1L);
+    }
+
+    @Test(expected = DomainError.class)
+    public void RemovedUserCannotLikePost() throws Exception {
+        createAll(AccountStatus.REMOVED);
+        blogService.addLikeToPost(2L, 1L);
+    }
 }
