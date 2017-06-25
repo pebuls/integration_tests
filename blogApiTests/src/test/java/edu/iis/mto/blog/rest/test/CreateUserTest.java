21980479727322d07069376f4f04ecd05c042450
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-06-25 21:39:36 +0200

    add test emailMustBeUnique n lass CreateUSerTest

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -1,11 +1,15 @@
 package edu.iis.mto.blog.rest.test;
 
+import edu.iis.mto.blog.rest.test.FunctionalTests;
+
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+
+import static javax.swing.text.DefaultStyledDocument.ElementSpec.ContentType;
 
 public class CreateUserTest extends FunctionalTests {
 
@@ -14,6 +18,14 @@ public class CreateUserTest extends FunctionalTests {
         JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user");
+    }
+
+    @Test
+    public void emailFieldMustBeUnique() throws Exception {
+        JSONObject jsonObject = new JSONObject().put("email", "john@domain.com").put("firstName", "Jan").put("lastName", "Bliski");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when()
                 .post("/blog/user");
     }
 }
